8a2d19d33385c56805fc3e9a66333d74d978892b
Kathy * <Kathy@KathyLambert.me>
2020-05-03 17:28:02 -0400

    Refractor(compareUserGuess): Add Bulma framework, user input element and remove prompts

--- scriptKL.js
+++ scriptKL.js
@@ -3,31 +3,43 @@
 // Guessing Game: Let the game generate a random number within a range of numbers (for example: between 0 and 10). Now you guess the number. You have 5 trials to guess the correct number. If the number you guessed is higher than the game-generated-number, then a message pops up saying it’s high else low or any message.
 // Use your imagination to add features to this basic game. Can add GUI or can make it playing online by passing to the server.
 
-// Create a function to generate a random number
-let maxNumberRange = 10
+// Declared and Initialized variables
+let inputElement = document.querySelector('.input')
+let counter = 0
 let maxGuesses = 5
+let maxNumberRange = 10
+
+// Create a function to generate a random number
 function getRandomNumber(maxNumberRange) {
   randomNumber = Math.floor(Math.random() * Math.floor(maxNumberRange))
   return randomNumber
 }
 console.log('randomNumber =', getRandomNumber(maxNumberRange))
 
-// Create a variable and assign it to the user input
-let userGuessInput = prompt('Please guess a random number')
-console.log('userGuessInput =', userGuessInput)
-let userNumber = Number(userGuessInput)
-console.log('randomNumber =', randomNumber)
-console.log('userNumber =', userNumber)
-
 // Compare user input to random number and alert user
 function compareUserGuess() {
-  if (userNumber == randomNumber) {
+  counter++
+  // Create a variable and assign it to the user input
+  let userGuessInput = inputElement
+  console.log('userGuessInput =', userGuessInput)
+  let userNumber = Number(userGuessInput)
+  console.log('randomNumber =', randomNumber)
+  console.log('userNumber =', userNumber)
+  if (counter < 6 && userNumber !== randomNumber) {
+    console.log('counter count: ', counter)
+    // console.log('Nooooo it is NOT a match')
+    // alert('Nooooo it is NOT a match')
+    counter += 1
+  }
+  if (userNumber === randomNumber) {
     console.log('Match')
     alert('Its a match')
-  } else {
-    console.log('Nooooo it is NOT a match')
-    alert('Nooooo it is NOT a match')
-    prompt('Please guess a random number')
+  } else if (counter === 6) {
+    console.log('No more guesses!')
+    alert('No more guesses!')
   }
+  counter++
+  console.log('counter count: ', counter)
+  // return false
 }
 compareUserGuess()
